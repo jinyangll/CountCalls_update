2d9635a72998b967fb25e86f36de06e30efde877
jinyangll <141242830+jinyangll@users.noreply.github.com>
2025-07-23 00:46:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import pandas as pd
-import json
-import os
+#from flask import Flask, request, jsonify
+#from flask_cors import CORS
+#import pandas as pd
+#import json
+#import os
 
 
 app = Flask(__name__)
